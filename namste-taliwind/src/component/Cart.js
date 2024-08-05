55b491459546e15589b79eaed0c411bea70b7d82
unnati1004 <unnatigandhi999@gmail.com>
2024-08-05 20:39:05 +0530

    update Cart

--- namste-taliwind/src/component/Cart.js
+++ namste-taliwind/src/component/Cart.js
@@ -1,14 +1,25 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/createSlice";
 
 const Cart = ()=>{
-   
+   const dispatch = useDispatch();
   const cartItems = useSelector((store)=>store.cart.items);        
   console.log(cartItems);
   
+const handleClearCart = ()=>{
+          dispatch(clearCart())
+}
+
+
  return <div className="text-center m-`4 p-4" >
   <h1 className="text-2xl font-bold" >Cart</h1>
-   <div>
+  <button className="bg-black rounded-lg p-2 text-white" 
+  onClick={handleClearCart}
+  >Clear Cart</button>
+
+  {cartItems.length == 0 && <h1>Cart is Empty. Add Items to your cart!</h1>}
+   <div className="w-6/12 m-auto">
      <ItemList items={cartItems}/>     
    </div>
  </div>
